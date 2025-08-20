89495fab4f40860fbe7d2827ac23a003605d8597
pokemmorgn <pokemmorgn@gmail.com>
2025-08-20 07:24:51 +0200

    Update User.ts

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -682,4 +682,3 @@ userSchema.methods.banWallet = async function (reason: string) {
 const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
 
 export default User;
-export type { IUser };
