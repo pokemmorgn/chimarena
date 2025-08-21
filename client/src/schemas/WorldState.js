951272d3aa809a95876a0ecfacfddda2bd523bc2
pokemmorgn <pokemmorgn@gmail.com>
2025-08-21 09:41:37 +0200

    Update WorldState.js

--- client/src/schemas/WorldState.js
+++ client/src/schemas/WorldState.js
@@ -2,8 +2,17 @@ import { Schema, type, MapSchema } from "@colyseus/schema";
 import { WorldPlayer } from "./WorldPlayer.js";
 
 export class WorldState extends Schema {
-  @type({ map: WorldPlayer }) players = new MapSchema();
-  @type("number") totalPlayers = 0;
-  @type("number") playersOnline = 0;
-  @type("number") playersSearching = 0;
+  constructor() {
+    super();
+    this.players = new MapSchema();
+    this.totalPlayers = 0;
+    this.playersOnline = 0;
+    this.playersSearching = 0;
+  }
 }
+
+// ⚡ Appliquer les annotations avec la syntaxe fonctionnelle
+type({ map: WorldPlayer })(WorldState.prototype, "players");
+type("number")(WorldState.prototype, "totalPlayers");
+type("number")(WorldState.prototype, "playersOnline");
+type("number")(WorldState.prototype, "playersSearching");
