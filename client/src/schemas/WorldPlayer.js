19e252c2a8d95409246f77d97c560f5667046536
pokemmorgn <pokemmorgn@gmail.com>
2025-08-21 09:41:26 +0200

    Update WorldPlayer.js

--- client/src/schemas/WorldPlayer.js
+++ client/src/schemas/WorldPlayer.js
@@ -1,14 +1,29 @@
 import { Schema, type } from "@colyseus/schema";
 
 export class WorldPlayer extends Schema {
-  @type("string") userId = "";
-  @type("string") username = "";
-  @type("number") level = 1;
-  @type("number") trophies = 0;
-  @type("number") currentArenaId = 0;
-  @type("string") status = "idle"; // idle, searching, in_battle
-  @type("number") lastSeen = Date.now();
-  @type("number") wins = 0;
-  @type("number") losses = 0;
-  @type("number") winRate = 0;
+  constructor() {
+    super();
+    this.userId = "";
+    this.username = "";
+    this.level = 1;
+    this.trophies = 0;
+    this.currentArenaId = 0;
+    this.status = "idle"; // idle, searching, in_battle
+    this.lastSeen = Date.now();
+    this.wins = 0;
+    this.losses = 0;
+    this.winRate = 0;
+  }
 }
+
+// ⚡ Appliquer les annotations avec la syntaxe fonctionnelle
+type("string")(WorldPlayer.prototype, "userId");
+type("string")(WorldPlayer.prototype, "username");
+type("number")(WorldPlayer.prototype, "level");
+type("number")(WorldPlayer.prototype, "trophies");
+type("number")(WorldPlayer.prototype, "currentArenaId");
+type("string")(WorldPlayer.prototype, "status");
+type("number")(WorldPlayer.prototype, "lastSeen");
+type("number")(WorldPlayer.prototype, "wins");
+type("number")(WorldPlayer.prototype, "losses");
+type("number")(WorldPlayer.prototype, "winRate");
